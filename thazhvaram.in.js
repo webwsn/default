f62cbf3faff0bae4c987d6f056867dadee8ac99d
webwsn <87135137+webwsn@users.noreply.github.com>
2021-07-08 20:08:27 +0530

    Update thazhvaram.in.js

--- thazhvaram.in.js
+++ thazhvaram.in.js
@@ -1,1 +1,1 @@
-(function(r,s){if(r.location!==r.top.location||r.location.href&&!r.location.href.match(/gmoj1\.in|192\.168/)){top.location.href="https://scriptbloggerblogspot.blogspot.com/"}})(window,document);
+
